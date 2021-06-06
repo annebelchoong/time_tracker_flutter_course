bc9107a97f99b863fb8b11a9b96da2b95f5518cf
annebelchoong <annebelchoong@gmail.com>
2021-06-06 19:09:55 +0800

    Lesson 140: Making logout button work by connecting it with firebase

--- lib/app/home_page.dart
+++ lib/app/home_page.dart
@@ -1,7 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key key}) : super(key: key);
+  const HomePage({Key key, @ required this.onSignOut}) : super(key: key);
+  final VoidCallback onSignOut;
+
+  Future<void> _signOut() async {
+    try {
+      await FirebaseAuth.instance.signOut();
+      onSignOut();
+    }catch(e){
+      print(e.toString());
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +22,7 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
         actions: <Widget>[
           TextButton(
-            onPressed: () {},
+            onPressed: _signOut,
             child: Text(
               'Logout',
               style: TextStyle(
