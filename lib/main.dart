1a11dfd18aed575ffcb0e769c7103358c327510a
annebelchoong <annebelchoong@gmail.com>
2021-06-06 16:24:41 +0800

    initialised the firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/sign_in_page.dart';
 
-void main() {
+void main() async{
+  //need to add this because calling to initialized before calling MyApp
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
