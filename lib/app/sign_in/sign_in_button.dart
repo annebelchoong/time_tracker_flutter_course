9f5571d60140c3555833ebdaeb6863686787bdfd
annebelchoong <annebelchoong@gmail.com>
2021-06-06 16:24:17 +0800

    remove non-nullable state

--- lib/app/sign_in/sign_in_button.dart
+++ lib/app/sign_in/sign_in_button.dart
@@ -3,14 +3,14 @@ import 'package:time_tracker_flutter_course/common_widgets/custom_raised_button.
 
 class SignInButton extends CustomRaisedButton {
   SignInButton({
-    required String? text,
-    Color? color,
-    Color? textColor,
-    VoidCallback? onPressed,
+    @required String text,
+    Color color,
+    Color textColor,
+    VoidCallback onPressed,
   })  : assert(text != null),
         super(
           child: Text(
-            text!,
+            text,
             style: TextStyle(
               color: textColor,
               fontSize: 15.0,
