b3526bed77f74ef6701155980cdc0d3f72d38903
annebelchoong <annebelchoong@gmail.com>
2021-06-09 23:46:04 +0800

    Lesson 214: Creating an AuthProvider

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:time_tracker_flutter_course/app/landing_page.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
+import 'package:time_tracker_flutter_course/services/auth_provider.dart';
 
 void main() async{
   //need to add this because calling to initialized before calling MyApp
@@ -13,14 +14,17 @@ void main() async{
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Time Tracker',
-      theme: ThemeData(
-        primarySwatch: Colors.orange,
-      ),
-      home:LandingPage(
-        auth: Auth(),
+    return AuthProvider(
+      auth: Auth(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Time Tracker',
+        theme: ThemeData(
+          primarySwatch: Colors.orange,
+        ),
+        home:LandingPage(
+          auth: Auth(),
+        ),
       ),
     );
   }
