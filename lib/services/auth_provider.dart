225e115fbc254974dfdf952d2c58a4e50027a90c
annebelchoong <annebelchoong@gmail.com>
2021-06-10 13:12:22 +0800

    Lesson 217: Using the Provider class
    Delete the auth_provider.dart by making all the code as a comment

--- lib/services/auth_provider.dart
+++ lib/services/auth_provider.dart
@@ -1,19 +1,19 @@
-import 'package:flutter/material.dart';
-import 'package:time_tracker_flutter_course/services/auth.dart';
-
-class AuthProvider extends InheritedWidget {
-  AuthProvider({@required this.auth, @required this.child});
-
-  final AuthBase auth;
-  final Widget child;
-
-  @override
-  bool updateShouldNotify(covariant InheritedWidget oldWidget) => false;
-
-//  To be able to use .of(context) like final auth = AuthProvider.of(context)
-  static AuthBase of(BuildContext context) {
-    AuthProvider provider =
-        context.dependOnInheritedWidgetOfExactType<AuthProvider>();
-    return provider.auth;
-  }
-}
+// import 'package:flutter/material.dart';
+// import 'package:time_tracker_flutter_course/services/auth.dart';
+//
+// class AuthProvider extends InheritedWidget {
+//   AuthProvider({@required this.auth, @required this.child});
+//
+//   final AuthBase auth;
+//   final Widget child;
+//
+//   @override
+//   bool updateShouldNotify(covariant InheritedWidget oldWidget) => false;
+//
+// //  To be able to use .of(context) like final auth = AuthProvider.of(context)
+//   static AuthBase of(BuildContext context) {
+//     AuthProvider provider =
+//         context.dependOnInheritedWidgetOfExactType<AuthProvider>();
+//     return provider.auth;
+//   }
+// }
