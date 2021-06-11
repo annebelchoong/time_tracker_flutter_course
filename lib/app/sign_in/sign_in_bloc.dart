db210fdc3b681970d47a53124753ed542bc88fe5
annebelchoong <annebelchoong@gmail.com>
2021-06-11 17:46:28 +0800

    Lesson 232: Implementing a simple BLoC
    1. Added a streamcontroller to access the stream
    2. Added a function to close the controller
    3. Added a funtion to add values to the stream

---
+++ lib/app/sign_in/sign_in_bloc.dart
@@ -0,0 +1,16 @@
+import 'dart:async';
+
+class SignInBloc{
+  //Added a stream controller to access the stream
+  final StreamController<bool> _isLoadingController = StreamController<bool>();
+  //input stream for the stream builder
+  //_isLoadingController is a private class which means the isLoadingStream can only access the stream but not the controller
+  Stream<bool> get isLoadingStream =>_isLoadingController.stream;
+
+  //close controller when the sign in page is moved from the widget tree
+  void dispose(){
+    _isLoadingController.close();
+  }
+  //Add values to the stream
+  void setIsLoading (bool isLoading) => _isLoadingController.add(isLoading);
+}
